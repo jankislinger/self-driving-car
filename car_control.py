15ffad484ab651c01b8138c973ac36f83128cbad
Jan Kislinger <jan.kislinger@gmail.com>
2017-10-31 20:41:08 +0100

    simple car control using keyboard

---
+++ car_control.py
@@ -0,0 +1,77 @@
+import GPIO
+import numpy as np
+
+GPIO.setup(GPIO.BOARD)
+
+
+class Car:
+
+    motor = Motor(7, 11)
+    servo = Servo(pin, frequency, dc_interval)
+
+    def forward(self):
+        print('Forward')
+        self.motor.forward()
+
+    def backward(self):
+        print('Backward')
+        self.motor.backward()
+
+    def stop(self):
+        print('Stopped')
+        self.motor.stop()
+
+    def left(self):
+        print('Left')
+        self.servo.left()
+
+    def right(self):
+        print('Right')
+        self.servo.right()
+
+    def straight(self):
+        print('Straight')
+        self.servo.straight()
+
+
+class Motor:
+
+    def __init__(self, forward_pin, backward_pin):
+        GPIO.setup(forward_pin, GPIO.OUT)
+        GPIO.setup(backward_pin, GPIO.OUT)
+        self.forward_pin = forward_pin
+        self.backward_pin = backward_pin
+
+    def forward(self):
+        GPIO.output(self.backward_pin, False)
+        GPIO.output(self.forward_pin, True)
+
+    def backward(self):
+        GPIO.output(self.forward_pin, False)
+        GPIO.output(self.backward_pin, True)
+
+    def stop(self):
+        GPIO.output(self.forward_pin, False)
+        GPIO.output(self.backward_pin, False)
+
+
+class Servo:
+
+    def __init__(self, pin, frequency, dc_interval):
+        self.pwm = GPIO.PWM(pin, frequency)
+        self.pwm.start(np.mean(dc_interval))
+        self.dc = dc_interval
+        self.dc_width = np.diff(dc_interval)[0]
+
+    def left(self):
+        self.pwm.ChangeDutyCycle(self.dc[0])
+
+    def right(self):
+        self.pwm.ChangeDutyCycle(self.dc[1])
+
+    def straight(self):
+        self.pwm.ChangeDutyCycle(np.mean(self.dc))
+
+    def turn(self, direction):
+        new_dc = self.dc[0] + (direction + 1) / 2 * self.dc_width
+        self.pwm.ChangeDutyCycle(new_dc)
---
+++ keyboard.py
@@ -0,0 +1,76 @@
+import pygame
+from car_control import *
+
+clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+
+x = 60
+y = 250
+
+going_left = False
+going_right = False
+going_up = False
+going_down = False
+
+running = True
+
+car = Car()
+
+try:
+    while running:
+        clock.tick(20)
+
+        screen.fill(WHITE)
+        pygame.draw.circle(screen, RED, [x, y], 40)
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    going_left = True
+                    going_right = False
+                if event.key == pygame.K_RIGHT:
+                    going_right = True
+                    going_left = False
+                if event.key == pygame.K_UP:
+                    going_up = True
+                    going_down = False
+                    car.forward()
+                if event.key == pygame.K_DOWN:
+                    going_down = True
+                    going_up = False
+                    car.backward()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    going_left = False
+                if event.key == pygame.K_RIGHT:
+                    going_right = False
+                if event.key == pygame.K_UP:
+                    going_up = False
+                    car.stop()
+                if event.key == pygame.K_DOWN:
+                    going_down = False
+                    car.stop()
+            elif event.type == pygame.QUIT:
+                running = False
+
+        if going_left:
+            x -= 1
+        elif going_right:
+            x += 1
+
+        if going_up:
+            y -= 1
+        elif going_down:
+            y += 1
+
+        pygame.display.flip()
+
+finally:
+    pygame.quit()
